e1ff9571e47af344c94e8986f32ed01809eaa5d6
dapomakanjuola <dm262@njit.edu>
2019-10-25 20:42:06 -0400

    feature:added css folder and registration form

---
+++ Registrationform.php
@@ -0,0 +1,41 @@
+<?php
+?>
+
+html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Registration Page</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+<form action="registrationpage.php" method="post">
+
+    <span>* required field.</span>
+    <br>
+    <input type="text" placeholder="Enter First Name" name="firstname">
+    <
+    <br>
+    <input type="text" placeholder="Enter Last Name" name="Lastname">
+
+    <br>
+    <input type="date" placeholder="Enter Birthday" name="birthday">
+
+    <br>
+    <input type="email" placeholder="Enter Email" name="email">
+
+    <br>
+    <input type="password" placeholder="Enter Password" name="password">
+
+    <br>
+    <input type="submit" name="submit">
+
+</form>
+
+</body>
+</html>
