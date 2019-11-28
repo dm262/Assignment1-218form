470e1a84a74829d70224822c041ba7f106f249ad
dapomakanjuola <dm262@njit.edu>
2019-11-28 11:36:20 -0500

    added styles for registration page

--- Registrationform.php
+++ Registrationform.php
@@ -1,35 +1,62 @@
-<?php include('form_test.php'); ?>
-<div>
+<?php
+include('form_test.php'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Registration</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/js/bootstrap.min.js" integrity="sha384-3qaqj0lc6sV/qpzrc1N5DC6i1VRn/HyX4qdPaiEFbn54VjQBEU341pvjz7Dv3n6P" crossorigin="anonymous"></script>
+</head>
+<body>
+<div></div>
+<div class="container-fluid">
+    <div class="jumbotron">
+        <h1 class="display-4">Registration</h1>
+        <p class="lead">Welcome to my assignment registration page</p>
+        <hr class="my-4">
+        <p>Enter all your information below</p>
+    </div>
+</div>
+<div class="container">
+
     <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
-        <h1>Registration</h1>
-        <fieldset>
-            <input placeholder="Your first name" type="text" name="firstname" value="<?= $firstname ?>"  >
-            <span><?= $firstname_error ?></span>
-        </fieldset>
+        <div class="form-group">
+            <label for="firstnname">First Name</label>
+            <input type="text" class="form-control"  name="firstname" value="<?= $firstname?>" aria-describedby="emailHelp" placeholder="Enter First name">
+            <span class="error"><?= $firstname_error ?></span>
+        </div>
+        <div class="form-group">
+            <label for="lastname">Last Name</label>
+            <input type="text" class="form-control"  name="lastname" value="<?= $lastname?>" aria-describedby="emailHelp" placeholder="Enter last name">
+            <span class="error"><?= $lastname_error ?></span>
+        </div>
+        <div class="form-group">
+            <label for="birthday">Birthday</label>
+            <input type="date" class="form-control"  name="birthday" value= "<?= $birthday?>" aria-describedby="emailHelp" placeholder="Enter your Birthday">
+            <span class="error"><?= $birthday_error ?></span>
+        </div>
 
-        <fieldset>
-            <input placeholder="Your last name" type="text" name="lastname" value="<?= $lastname ?>"  >
-            <span><?= $lastname_error ?></span>
-        </fieldset>
+        <div class="form-group">
+            <label for="email">Email address </label>
+            <input type="text" class="form-control"  name="email" value="<?= $email?>" aria-describedby="emailHelp" placeholder="Enter email ">
+            <span class="error"><?= $email_error ?></span>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control"  name = password value="<?= $password?>" placeholder="Password">
+            <span class="error"><?= $password_error ?></span>
+        </div>
 
-
-        <fieldset>
-            <input placeholder="Your Email Address" type="text" name="email" value="<?= $email ?>"  >
-            <span><?= $email_error ?></span>
-        </fieldset>
-
-        <fieldset>
-            <input placeholder="Birthday" type="date" name="birthday" value="<?= $birthday ?>"  >
-            <span><?= $birthday_error ?></span>
-        </fieldset>
-
-        <fieldset>
-            <input placeholder="Password" type="password" name="password" value="<?= $password ?>"  >
-            <span><?= $password_error ?></span>
-        </fieldset>
-
-        <button name="submit" type="submit"  data-submit="...Sending">Submit</button>
-
-        <div><?= $success ?></div>
-
+        <button type="submit" name="submit" class="btn btn-primary">Register</button>
+        <span><?=$success?></span>
     </form>
+
+
+
+
+</div>
+
+</body>
+</html>
